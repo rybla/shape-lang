a90d4a26aff292f9c0664d1963303639b273a170
Henry Blanchette <henry@monkeyface.com>
2022-01-31 16:35:03 -0500

    reformatted grammar def

--- src/shape/Grammar.ts
+++ src/shape/Grammar.ts
@@ -1,20 +1,82 @@
 import { List } from "immutable"
 
-export type Context = List<[Label, Term , undefined | Term]>
+// debruijnlevel => [label, type, value?]
+export type Context = List<[Label, Term , Term | undefined]>
 
-export type Term
-  = {case: "uni"}
-  | {case: "pi", label: Label, dom: Term, cod: Term}
-  | {case: "lam", label: Label, dom: Term, bod: Term}
-  | {case: "let", label: Label, dom: Term, arg: Term, bod: Term}
-  | {case: "app", app: Term, arg: Term}
-  | {case: "var", dbl: Dbl}
-  | {case: "hole"}
+export type Substitution<A> = List<[A, Term]>;
+
+export type Term = TermPi | TermLambda | TermLet | TermApplication | TermVariable | TermHole
+
+export type TermPi = {
+  case: "pi",
+  label: Label,
+  domain: Term,
+  codomain: Term,
+  format?: {
+    indented: boolean,
+    annotated: boolean
+  }
+}
+
+export type TermLambda = {
+  case: "lambda",
+  label: Label,
+  domain: Term,
+  body: Term,
+  format?: {
+    indented: boolean,
+    annotated: boolean
+  }
+}
+
+export type TermLet = {
+  case: "let",
+  label: Label,
+  domain: Term,
+  argument: Term,
+  body: Term,
+  format?: {
+    indented: boolean,
+    annotated: boolean
+  }
+}
+
+export type TermApplication = {
+  case: "application",
+  applicant: Term,
+  argument: Term,
+  format?: {
+    indented: boolean
+  }
+}
+
+export type TermVariable = {
+  case: "variable",
+  debruijnlevel: DeBruijnLevel,
+  format?: {
+    // TODO
+  }
+}
+
+export type TermHole = {
+  case: "hole",
+  hole: Hole,
+  weakening: DeBruijnLevel,
+  substitution: Substitution<DeBruijnLevel>
+  format?: {
+    // TODO
+  }
+}
 
 // A term index specifies a node in a Term AST.
 export type TermIx = {} // TODO
 
 // DeBruijn level
-export type Dbl = number;
+export type DeBruijnLevel = number;
 
+// Variable label
 export type Label = {value: string};
+
+// Hole Id
+export type Hole = {holeId: HoleId}
+export type HoleId = number;
