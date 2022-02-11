34851d45b0ddd4d33b00efba4fab9cb8b57c32e8
Henry Blanchette <henry@monkeyface.com>
2022-02-10 22:33:01 -0500

    updating syntax to plc

--- src/shape/Syntax.ts
+++ src/shape/Syntax.ts
@@ -1,50 +1,94 @@
 import { List } from "immutable";
 
+// Program
+
+export type Program = List<Declaration>
+
+export type Declaration = DefineTerm | DefineData
+
+export type DefineTerm = {
+  label: Label,
+  signature: Type, 
+  value: Term
+}
+
+export type DefineData = {
+  label: Label,
+  parameters: List<Type>,
+  constructors: List<Constructor>
+}
+
+export type Constructor = {
+  label: Label,
+  parameters: List<Type>,
+  codomain: Type
+}
+
+// Block
+
 export type Block = {
-  bindings: List<{label: Label, signature: Term, value: Term}>,
+  bindings: List<{label: Label, signature: Type, value: Term}>,
   body: Term,
   format: Format
 }
 
-export type Term = Universe | Pi | Lambda |  Neutral | Hole
+// Term
+
+export type Term = Lambda |  Neutral | Match | Hole
 
 export type Lambda = {
   case: "lambda",
-  parameters: List<{label: Label, domain: Term}>,
+  parameters: List<{label: Label, domain: Type}>,
   body: Block,
-  format: Format
-}
-
-export type Universe = {
-  case: "universe",
-  level: number,
-  format: Format
-}
-
-export type Pi = {
-  case: "pi",
-  parameters: List<{label: Label, domain: Term}>,
-  codomain: Block,
   format: Format
 }
 
 export type Neutral = {
   case: "neutral",
-  applicant: DeBruijn,
+  applicant: Label,
   arguments: List<Term>,
   format: Format
+}
+
+export type Match = {
+  case: "match",
+  argument: Term,
+  branches: List<[Pattern, Term]>
+}
+
+export type Pattern = {
+  label: Label,
+  paramaters: List<Label>
 }
 
 export type Hole = {
   case: "hole",
   holeId: Symbol,
-  weakening: DeBruijn,
-  substitution: Substitution<DeBruijn, Term>,
+  weakening: List<Label>,
+  substitution: Substitution<Label, Term>,
   format: Format
 }
 
-// Variables references are DeBruijn levels
-export type DeBruijn = number
+// Type
+
+export type Type = Base | Arrow | Data 
+
+export type Base = {
+  case: "basic",
+  subcase: "unit" | "boolean" | "natural" | "integer"
+}
+
+export type Arrow = {
+  case: "arrow",
+  arguments: List<Type>,
+  codomain: Type
+}
+
+export type Data = {
+  case: "data",
+  label: Label, 
+  arguments: List<Type>
+}
 
 export type Label = {value: string}
 
@@ -57,7 +101,7 @@ export function freshHole(): Hole {
   return {
     case: "hole",
     holeId,
-    weakening: 0,
+    weakening: List(),
     substitution: List(),
     format: defaultFormat()
   }
@@ -90,43 +134,47 @@ export type SubstitutionItem<A, B> = [A, B]
 
 // Index
 
-export type Index = List<IndexStep>
+export type Index = List<{}>
 
-export type IndexStep =
-  | {
-      case: "block",
-      sub:
-        | {
-            case: "binding",
-            i: number,
-            sub: {case: "label"} | {case: "signature"} | {case: "value"},
-            next: Index
-          }
-        | {case: "body"}
-    }
-  | {
-      case: "pi",
-      sub:
-        | {
-            case: "parameter",
-            i: number,
-            sub: {case: "label"} | {case: "signature"}
-          }
-        | {case: "codomain"}
-    }
-  | {
-      case: "lambda",
-      sub:
-        | {
-            case: "parameter",
-            i: number,
-            sub: {case: "label"} | {case: "signature"}
-          }
-        | {case: "body"}
-    }
-  | {
-      case: "neutral",
-      sub:
-        | {case: "applicant"}
-        | {case: "argument", i: number}
-    }
+// TODO: merge indexing with changes to grammar
+
+// export type Index = List<IndexStep>
+
+// export type IndexStep =
+//   | {
+//       case: "block",
+//       sub:
+//         | {
+//             case: "binding",
+//             i: number,
+//             sub: {case: "label"} | {case: "signature"} | {case: "value"},
+//             next: Index
+//           }
+//         | {case: "body"}
+//     }
+//   | {
+//       case: "pi",
+//       sub:
+//         | {
+//             case: "parameter",
+//             i: number,
+//             sub: {case: "label"} | {case: "signature"}
+//           }
+//         | {case: "codomain"}
+//     }
+//   | {
+//       case: "lambda",
+//       sub:
+//         | {
+//             case: "parameter",
+//             i: number,
+//             sub: {case: "label"} | {case: "signature"}
+//           }
+//         | {case: "body"}
+//     }
+//   | {
+//       case: "neutral",
+//       sub:
+//         | {case: "applicant"}
+//         | {case: "argument", i: number}
+//     }
