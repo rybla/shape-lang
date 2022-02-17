4b48ad2c5866ec8b6c792ebb2ae15612ded707b1
Henry Blanchette <henry@monkeyface.com>
2022-02-17 10:32:53 -0500

    rewording, formatting

--- src/lang/model.ts
+++ src/lang/model.ts
@@ -9,17 +9,33 @@ export function removeConstructor(module: Module, index: ModuleIndex): Module {t
 // Remove the binding at `index`.
 export function removeBinding(module: Module, index: ModuleIndex): Module {throw new Error()}
 
-// Insert `domain` at `domainIndex` in the type of the name bound at `parentIndex`.
-// Note that `domainIndex` is relative to the top of the type of the binding at `parentIndex`.
-export function insertDomain(module: Module, parentIndex: ModuleIndex, domain: Type, domainIndex: TypeIndex): Module {throw new Error()}
+// Insert `domain` at `domainIndex` relative to the top of the type of the
+// binding at `parentIndex`.
+export function insertDomain(
+  module: Module,
+  parentIndex: ModuleIndex,
+  domain: Type,
+  domainIndex: TypeIndex
+): Module {throw new Error()}
 
-// Remove the domain at `domainIndex` in the type of the name bound at `parentIndex`.
-// Note that `domainIndex` is relative to the top of the type of the binding at `parentIndex`.
-export function removeDomain(module: Module, parentIndex: ModuleIndex, domainIndex: TypeIndex): Module {throw new Error()}
+// Remove the domain at `domainIndex` relative to the top of the type of the
+// binding at `parentIndex`.
+export function removeDomain(
+  module: Module,
+  parentIndex: ModuleIndex,
+  domainIndex: TypeIndex
+): Module {throw new Error()}
 
-// Move the domain at `sourceDomainIndex` to `targetDomainIndex`.
-// Note that `domainIndex` is relative to the top of the type of the binding at `parentIndex`.
-export function moveDomain(module: Module, parentIndex: ModuleIndex, sourceDomainIndex: TypeIndex, targetDomainIndex: TypeIndex): Module {throw new Error()}
+// Move the domain at `sourceDomainIndex` to `targetDomainIndex`, which are
+// domains of the arrow type at `arrowIndex`, relative to the top of the type of
+// the binding at `parentIndex`.
+export function moveDomain(
+  module: Module,
+  parentIndex: ModuleIndex,
+  arrowIndex: TypeIndex,
+  sourceDomainIndex: number,
+  targetDomainIndex: number
+): Module {throw new Error()}
 
 // Fill the hole at `index` with `term`
 export function fillHole(module: Module, index: Index, term: Term): Module {throw new Error()}
