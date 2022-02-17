bbb507629e972f491e3c4423de85f34704ca2a46
Henry Blanchette <henry@monkeyface.com>
2022-02-17 10:15:08 -0500

    reordered arguments

--- src/lang/model.ts
+++ src/lang/model.ts
@@ -11,15 +11,15 @@ export function removeBinding(module: Module, index: Index): Module {throw new E
 
 // Insert `domain` at `domainIndex` in the type of the name bound at `bindingIndex`.
 // Note that `domainIndex` is relative to the top of the type of the binding at `bindingIndex`.
-export function insertDomain(module: Module, domain: Type, domainIndex: TypeIndex, bindingIndex: BindingIndex): Module {throw new Error()}
+export function insertDomain(module: Module, bindingIndex: BindingIndex, domain: Type, domainIndex: TypeIndex): Module {throw new Error()}
 
 // Remove the domain at `domainIndex` in the type of the name bound at `bindingIndex`.
 // Note that `domainIndex` is relative to the top of the type of the binding at `bindingIndex`.
-export function removeDomain(module: Module, domainIndex: TypeIndex, bindingIndex: BindingIndex): Module {throw new Error()}
+export function removeDomain(module: Module, bindingIndex: BindingIndex, domainIndex: TypeIndex): Module {throw new Error()}
 
 // Move the domain at `sourceDomainIndex` to `targetDomainIndex`.
 // Note that `domainIndex` is relative to the top of the type of the binding at `bindingIndex`.
-export function moveDomain(module: Module, sourceDomainIndex: TypeIndex, targetDomainIndex: TypeIndex, bindingIndex: BindingIndex): Module {throw new Error()}
+export function moveDomain(module: Module, bindingIndex: BindingIndex, sourceDomainIndex: TypeIndex, targetDomainIndex: TypeIndex): Module {throw new Error()}
 
 // Fill the hole at `index` with `term`
 export function fillHole(module: Module, index: Index, term: Term): Module {throw new Error()}
