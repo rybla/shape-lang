335aa32846c274808e90fd2fdc3bc5a03de661df
Henry Blanchette <henry@monkeyface.com>
2022-02-18 15:22:55 -0500

    polished grammar

--- design/stlc/Model.hs
+++ design/stlc/Model.hs
@@ -4,26 +4,42 @@ import Symbol
 
 type Program = Block
 
-data Type = ArrowType [(Name, Type)] (Either DataType HoleType) | DataType DataType | HoleType HoleType
-
-type DataType = Symbol
-
-type HoleType = Hole
-
 data Block = Block [Definition] Term
 
 data Definition
-  = TermDefinition (Name, Symbol) Type Term
-  | DataDefinition (Name, Symbol) [Constructor]
+  = TermDefinition Binding Type Term
+  | DataDefinition Binding [Constructor]
 
-data Constructor = Constructor (Name, Symbol) [Type]
+data Type
+  = ArrowType [Name] (Either Reference Hole)
+  | DataType Reference
+  | HoleType Hole
 
-data Term = LambdaTerm [Symbol] Block | NeutralTerm Symbol [Term] | HoleTerm
+data Constructor = Constructor Binding [Type]
 
-type HoleTerm = Hole
+data Term
+  = LambdaTerm [Id] Block
+  | NeutralTerm Id [Term]
+  | HoleTerm Hole Weakening Substitution
 
-type Hole = Symbol
+-- Binding & Reference
 
-type Name = String
+type Binding = (Name, Id)
 
---
+type Reference = Id
+
+-- Name & Id
+
+type Name = String -- not necessarily unique
+
+type Id = Symbol -- unique
+
+-- Hole
+
+type Hole = Id
+
+-- Weakening & Substitution
+
+data Weakening -- TODO
+
+data Substitution -- TODO
