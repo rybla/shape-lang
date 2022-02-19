95e05c8e8ba42708f43e6028f123c8ccd1d65446
Henry Blanchette <henry@monkeyface.com>
2022-02-19 13:18:49 -0500

    cleaned up syntax, remove unnecessary complication

--- design/stlc/Model.hs
+++ design/stlc/Model.hs
@@ -7,32 +7,28 @@ type Program = Block
 data Block = Block [Definition] Term
 
 data Definition
-  = TermDefinition Binding Type Term
-  | DataDefinition Binding [Constructor]
+  = TermDefinition (Name, Id) Type Term
+  | DataDefinition (Name, Id) [Constructor]
 
 data Type
-  = ArrowType [Parameter] (Either Reference Hole)
+  = ArrowType [(Name, Type)] (Either Reference Hole)
   | DataType Reference
   | HoleType Hole
 
-data Constructor = Constructor Binding [(Name, Type)]
+data Constructor = Constructor (Name, Id) [(Name, Type)]
 
 data Term
   = LambdaTerm [Id] Block
   | NeutralTerm Id [Term]
+  | MatchTerm
+      Reference -- type of term to be matched on
+      Term -- term to be matched on
+      [([Id], Term)] -- cases of matched term
   | HoleTerm Hole Weakening Substitution
-  | MatchTerm Reference [([Reference], Term)] Term
---            ^ the type  ^ args       ^ case ^ term to be matched on
 
--- Parameter, Binding, Reference
-
-type Parameter = (Name, Type)
-
-type Binding = (Name, Id)
+-- Reference, Name, Id
 
 type Reference = Id
-
--- Name & Id
 
 type Name = String -- not necessarily unique
 
