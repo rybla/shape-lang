c8d60a151164c43a1648b3b768594e1059ca4da2
Henry Blanchette <henry@monkeyface.com>
2022-02-19 13:38:40 -0500

    re-intro Parameter, intro Case

--- design/stlc/Model.hs
+++ design/stlc/Model.hs
@@ -7,24 +7,35 @@ type Program = Block
 data Block = Block [Definition] Term
 
 data Definition
-  = TermDefinition (Name, Id) Type Term
-  | DataDefinition (Name, Id) [Constructor]
+  = TermDefinition (Id, Name) Type Term
+  | DataDefinition Id [Constructor]
 
-data Constructor = Constructor (Name, Id) [(Name, Type)]
+data Constructor = Constructor Id [Parameter]
 
 data Type
-  = ArrowType [(Name, Type)] (Either Reference Hole)
+  = ArrowType [Parameter] (Either Reference Hole)
   | DataType Reference
   | HoleType Hole
 
 data Term
-  = LambdaTerm [Id] Block
+  = LambdaTerm [Id] Block -- the ids are specified in its `ArrowType`
   | NeutralTerm Reference [Term]
   | MatchTerm
-      Reference -- type of term to be matched on
-      Term -- term to be matched on
-      [([Id], Block)] -- cases of matched term
+      Reference -- type of term to match on (must be a DataType)
+      Term -- term to match on
+      [Case] -- cases of matched term
   | HoleTerm Hole Weakening Substitution
+
+data Case
+  = Case
+      Id -- of the `Constructor`
+      [Id] -- instances of the `Constructor`'s `Parameter`s
+      Block
+
+-- Parameter
+
+-- A `Parameter` appears where the type of a function specifies the name of a `Parameter` and its `Type`, as in `ArrowType` or `Constructor`. No `Id` is specified since this is not an instance of the `Name` as a term. The same `Parameter`'s `Name` could be instantiated multiple times, such as in distinct `LambdaTerm`s and `MatchTerm` cases.
+type Parameter = (Name, Type)
 
 -- Reference, Name, Id
 
