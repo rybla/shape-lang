47d1e3c807b536fe7f9e50eb9e7ac627e1905a49
Henry Blanchette <henry@monkeyface.com>
2022-02-19 13:47:20 -0500

    Binding and UniqueBinding

--- design/stlc/Model.hs
+++ design/stlc/Model.hs
@@ -7,10 +7,10 @@ type Program = Block
 data Block = Block [Definition] Term
 
 data Definition
-  = TermDefinition (Id, Name) Type Term
-  | DataDefinition Id [Constructor]
+  = TermDefinition UniqueBinding Type Term
+  | DataDefinition UniqueBinding [Constructor]
 
-data Constructor = Constructor (Name, Id) [Parameter]
+data Constructor = Constructor UniqueBinding [Parameter]
 
 data Type
   = ArrowType [Parameter] (Either Reference Hole)
@@ -18,7 +18,7 @@ data Type
   | HoleType Hole
 
 data Term
-  = LambdaTerm [Id] Block -- the ids are specified in its `ArrowType`
+  = LambdaTerm [Binding] Block -- the ids are specified in its `ArrowType`
   | NeutralTerm Reference [Term]
   | MatchTerm
       Reference -- type of term to match on (must be a DataType)
@@ -28,14 +28,20 @@ data Term
 
 data Case
   = Case
-      Id -- of the `Constructor`
-      [Id] -- instances of the `Constructor`'s `Parameter`s
+      Reference -- to the `Constructor`
+      [Binding] -- instances of the `Constructor`'s `Parameter`s
       Block
 
--- Parameter
+-- Parameter, Binding, UniqueBinding
 
 -- A `Parameter` appears where the type of a function specifies the name of a `Parameter` and its `Type`, as in `ArrowType` or `Constructor`. No `Id` is specified since this is not an instance of the `Name` as a term. The same `Parameter`'s `Name` could be instantiated multiple times, such as in distinct `LambdaTerm`s and `MatchTerm` cases.
 type Parameter = (Name, Type)
+
+-- A `Binding` appears where an instance of a `Name` is bound, as in `LambdaTerm` and `Case`. The `Name` that is bound is contextually determined, by a `ArrowType` and `Constructor` respectively.
+type Binding = Id
+
+-- A `UniqueBinding` appears where a `Name` is introduced and a unique instance of that `Name` is bound at once.
+type UniqueBinding = (Name, Id)
 
 -- Reference, Name, Id
 
