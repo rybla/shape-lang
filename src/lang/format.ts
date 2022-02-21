26985acbbeb07c9467c57b4b90dd5ba5a70b295e
Henry Blanchette <henry@monkeyface.com>
2022-02-20 20:55:59 -0500

    updated format to match syntax

--- src/lang/format.ts
+++ src/lang/format.ts
@@ -1,6 +1,6 @@
 // Format
 
-import { Label, Syntax } from "./syntax"
+import { Binding, Name, Reference, Syntax } from "./syntax"
 
 export type FormatField = "indented" | "unannotated"
 export type FormatData = { [K in FormatField]?: boolean }
@@ -8,7 +8,7 @@ export type FormatData = { [K in FormatField]?: boolean }
 export type Format<S extends Syntax> =
   {
     [Key in keyof S]:
-      S[Key] extends Label ? S :
+      S[Key] extends (Binding | Name | Reference) ? S :
       S[Key] extends (infer T)[] ? (T extends Syntax ? Format<T>[] : T[]) :
       S[Key] extends Syntax ? Format<S[Key]> : S[Key]
   } 
