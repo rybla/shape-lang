cd3fcd42108bbc7744aa77243f5b73731ba99545
Henry Blanchette <henry@monkeyface.com>
2022-02-20 20:56:11 -0500

    commented out WIP for index and model

--- src/lang/index.ts
+++ src/lang/index.ts
@@ -1,218 +1,220 @@
-import { Map } from "immutable"
-import { ArrowType, Binding, Block, Constructor, Context, DataType, Definition, LambdaTerm, Name, Parameter, Reference, Syntax, Term, Type } from "./syntax"
+export type Index = {}
 
-export function lookupAt<S extends Syntax, T extends Syntax>(source: S, index: Index<S>, gamma: Context): {target: T, gamma: Context} {
-  throw new Error("unimplemented")
-}
+// import { Map } from "immutable"
+// import { ArrowType, Binding, Block, Constructor, Context, DataType, Definition, LambdaTerm, Name, Parameter, Reference, Syntax, Term, Type } from "./syntax"
 
-export function replaceAt<S extends Syntax, T extends Syntax>(source: S, index: Index<S>, gamma: Context, replace: (target: T, gamma: Context) => T): S {
-  throw new Error("unimplemented")
-}
+// export function lookupAt<S extends Syntax, T extends Syntax>(source: S, index: Index<S>, gamma: Context): {target: T, gamma: Context} {
+//   throw new Error("unimplemented")
+// }
 
-export type HereIndex = {readonly case: "here"}
-export const here: HereIndex = {case: "here"}
+// export function replaceAt<S extends Syntax, T extends Syntax>(source: S, index: Index<S>, gamma: Context, replace: (target: T, gamma: Context) => T): S {
+//   throw new Error("unimplemented")
+// }
 
-export type Indexable = IndexStepable | IndexTerminal
+// export type HereIndex = {readonly case: "here"}
+// export const here: HereIndex = {case: "here"}
 
-export type IndexStepable = 
-  | Block
-  | Definition
-  | Constructor
-  | Type
-  | Term
-  | Parameter
-export type IndexTerminal =
-  | Binding
-  | Name
-  | Reference
+// export type Indexable = IndexStepable | IndexTerminal
 
-type IndexStep_<S extends IndexStepable, P extends keyof S> = 
-  S[P] extends (infer Q)[] ? (Q extends Indexable ? [P, number, Index<Q>] : never) :
-  S[P] extends Indexable ? [P, Index<S[P]>] :
-  never
-type IndexStep<S extends IndexStepable> = 
-  <R>(run: <P extends keyof S>(_: IndexStep_<S, P>) => R) => R
+// export type IndexStepable = 
+//   | Block
+//   | Definition
+//   | Constructor
+//   | Type
+//   | Term
+//   | Parameter
+// export type IndexTerminal =
+//   | Binding
+//   | Name
+//   | Reference
 
-const indexStep =
-  <S extends IndexStepable>() =>
-  <P extends keyof S>(indexStep_: IndexStep_<S, P>): IndexStep<S> =>
-  (run) =>
-  run(indexStep_)
+// type IndexStep_<S extends IndexStepable, P extends keyof S> = 
+//   S[P] extends (infer Q)[] ? (Q extends Indexable ? [P, number, Index<Q>] : never) :
+//   S[P] extends Indexable ? [P, Index<S[P]>] :
+//   never
+// type IndexStep<S extends IndexStepable> = 
+//   <R>(run: <P extends keyof S>(_: IndexStep_<S, P>) => R) => R
 
-export type Index<S extends Indexable> =
-  | HereIndex
-  // @ts-ignore
-  | S extends IndexStepable ? IndexStep<S> : HereIndex
+// const indexStep =
+//   <S extends IndexStepable>() =>
+//   <P extends keyof S>(indexStep_: IndexStep_<S, P>): IndexStep<S> =>
+//   (run) =>
+//   run(indexStep_)
+
+// export type Index<S extends Indexable> =
+//   | HereIndex
+//   // @ts-ignore
+//   | S extends IndexStepable ? IndexStep<S> : HereIndex
   
-// ! doesn't work...
-// let i: Index<DataType> =
-//     indexStep<DataType>()<"reference">(["reference", here])
-// let j: Index<ArrowType> =
-//   indexStep<ArrowType>()<"parameters">(["parameters", 0, 
-//   indexStep<Parameter>()<"name">(["name", here])])
+// // ! doesn't work...
+// // let i: Index<DataType> =
+// //     indexStep<DataType>()<"reference">(["reference", here])
+// // let j: Index<ArrowType> =
+// //   indexStep<ArrowType>()<"parameters">(["parameters", 0, 
+// //   indexStep<Parameter>()<"name">(["name", here])])
 
-  // | (S extends Reference ? HereIndex :
-  //   <P extends keyof S>(p: P) =>
-  //     S[P] extends (infer Q)[] ? (Q extends Syntax ? [number, Index<Q>] : never) :
-  //     S[P] extends Syntax ? Index<S[P]> : never)
+//   // | (S extends Reference ? HereIndex :
+//   //   <P extends keyof S>(p: P) =>
+//   //     S[P] extends (infer Q)[] ? (Q extends Syntax ? [number, Index<Q>] : never) :
+//   //     S[P] extends Syntax ? Index<S[P]> : never)
 
-// let x: Reference = undefined as unknown as Reference
-// let _ = lookupAt<LambdaTerm, LambdaTerm>(
-//   {case: "lambda", body: {case: "block", definitions: [], body: {case: "neutral", reference: x, args: []}}},
-//   undefined as unknown as Index<LambdaTerm>,
-//   Map()
-// )
+// // let x: Reference = undefined as unknown as Reference
+// // let _ = lookupAt<LambdaTerm, LambdaTerm>(
+// //   {case: "lambda", body: {case: "block", definitions: [], body: {case: "neutral", reference: x, args: []}}},
+// //   undefined as unknown as Index<LambdaTerm>,
+// //   Map()
+// // )
 
-// const i: Index<DataType> = 
-//   (): ["reference", Index<DataType["reference"]>] =>
-//   ["reference", here]
+// // const i: Index<DataType> = 
+// //   (): ["reference", Index<DataType["reference"]>] =>
+// //   ["reference", here]
 
-// const j = i
+// // const j = i
 
 
-// (_:"body"): Index<LambdaTerm["body"]> => undefined as unknown as Index<LambdaTerm["body"]>
+// // (_:"body"): Index<LambdaTerm["body"]> => undefined as unknown as Index<LambdaTerm["body"]>
 
-// lookupAt<Module, Term>(undefined as unknown as Module, () => ({key: "statements", index: () => ({key: ""})}))
+// // lookupAt<Module, Term>(undefined as unknown as Module, () => ({key: "statements", index: () => ({key: ""})}))
 
-// type IsVariant<A> = A extends (keyof (infer B)) ? "true" : "false"
-// function f<A, B>(x: IsVariant<A | B>): void {}
-// f()
+// // type IsVariant<A> = A extends (keyof (infer B)) ? "true" : "false"
+// // function f<A, B>(x: IsVariant<A | B>): void {}
+// // f()
 
-  // export type IndexStep<S extends Syntax, P extends keyof S> =
-  //   [P, ]
+//   // export type IndexStep<S extends Syntax, P extends keyof S> =
+//   //   [P, ]
 
-    // {
-    //   [Key in keyof S]:
-    //     S[Key] extends (infer T)[] ? unknown :
-    //     S[Key] extends Syntax ? Index<S> :
-    //     never
-    // }
+//     // {
+//     //   [Key in keyof S]:
+//     //     S[Key] extends (infer T)[] ? unknown :
+//     //     S[Key] extends Syntax ? Index<S> :
+//     //     never
+//     // }
 
-  // S extends Label ? HereIndex :
-  // S extends Module ? 
+//   // S extends Label ? HereIndex :
+//   // S extends Module ? 
 
-// export type Index<S extends Syntax> = 
-//   {
-//     [Key in keyof S]:
-//       S extends Label ? HereIndex :
-//       S[Key] extends (infer T)[] ? (T extends Syntax ? {i: number, index: Index<T>} : unknown) :
-//       S[Key] extends Module ? S[Key] : // TODO
-//       S[Key] extends Statement ? S[Key] : // TODO
-//       S[Key] extends Constructor ? S[Key] : // TODO
-//       S[Key] extends Type ? S[Key] : // TODO
-//       S[Key] extends Block ? S[Key] : // TODO
-//       S[Key] extends Binding ? S[Key] : // TODO
-//       S[Key] extends Term ? S[Key] : // TODO
-//       S[Key] extends Parameter ? S[Key] : // TODO
-//       unknown
-//   }
+// // export type Index<S extends Syntax> = 
+// //   {
+// //     [Key in keyof S]:
+// //       S extends Label ? HereIndex :
+// //       S[Key] extends (infer T)[] ? (T extends Syntax ? {i: number, index: Index<T>} : unknown) :
+// //       S[Key] extends Module ? S[Key] : // TODO
+// //       S[Key] extends Statement ? S[Key] : // TODO
+// //       S[Key] extends Constructor ? S[Key] : // TODO
+// //       S[Key] extends Type ? S[Key] : // TODO
+// //       S[Key] extends Block ? S[Key] : // TODO
+// //       S[Key] extends Binding ? S[Key] : // TODO
+// //       S[Key] extends Term ? S[Key] : // TODO
+// //       S[Key] extends Parameter ? S[Key] : // TODO
+// //       unknown
+// //   }
 
-// export type Index = ModuleIndex | StatementIndex | ConstructorIndex | TypeIndex | TermIndex | BlockIndex | BindingIndex | ParameterIndex
+// // export type Index = ModuleIndex | StatementIndex | ConstructorIndex | TypeIndex | TermIndex | BlockIndex | BindingIndex | ParameterIndex
 
-// export type ModuleIndex = {}
-// export type StatementIndex = {}
-// export type ConstructorIndex = {}
-// export type TypeIndex = {}
-// export type TermIndex = {}
-// export type BlockIndex = {}
-// export type BindingIndex = {}
-// export type ParameterIndex = {}
+// // export type ModuleIndex = {}
+// // export type StatementIndex = {}
+// // export type ConstructorIndex = {}
+// // export type TypeIndex = {}
+// // export type TermIndex = {}
+// // export type BlockIndex = {}
+// // export type BindingIndex = {}
+// // export type ParameterIndex = {}
 
-// // ModuleIndex
+// // // ModuleIndex
 
-// export type ModuleIndex = IndexStep<{case: "module", i: number, index: StatementIndex}>
+// // export type ModuleIndex = IndexStep<{case: "module", i: number, index: StatementIndex}>
 
-// // StatementIndex
+// // // StatementIndex
 
-// export type StatementIndex = IndexStep<TermDefinitionIndex | DataDefinitionIndex>
-// export type TermDefinitionIndex = 
-//   {
-//     case: "term definition",
-//     sub:
-//       | {case: "label"}
-//       | {case: "type", index: TypeIndex}
-//       | {case: "term", index: TermIndex}
-//   }
-// export type DataDefinitionIndex =
-//   {
-//     case: "data definition",
-//     sub:
-//       | {case: "label"}
-//       | {case: "constructor", i: number, index: ConstructorIndex}
-//   }
+// // export type StatementIndex = IndexStep<TermDefinitionIndex | DataDefinitionIndex>
+// // export type TermDefinitionIndex = 
+// //   {
+// //     case: "term definition",
+// //     sub:
+// //       | {case: "label"}
+// //       | {case: "type", index: TypeIndex}
+// //       | {case: "term", index: TermIndex}
+// //   }
+// // export type DataDefinitionIndex =
+// //   {
+// //     case: "data definition",
+// //     sub:
+// //       | {case: "label"}
+// //       | {case: "constructor", i: number, index: ConstructorIndex}
+// //   }
 
-// // ConstructorIndex
+// // // ConstructorIndex
 
-// export type ConstructorIndex =
-//   IndexStep<{
-//     case: "constructor", 
-//     sub: ConstructorSubIndex
-//   }>
-// export type ConstructorSubIndex =
-//   | {case: "label"}
-//   | {case: "domain", i: number, index: TypeIndex}
+// // export type ConstructorIndex =
+// //   IndexStep<{
+// //     case: "constructor", 
+// //     sub: ConstructorSubIndex
+// //   }>
+// // export type ConstructorSubIndex =
+// //   | {case: "label"}
+// //   | {case: "domain", i: number, index: TypeIndex}
 
-// // TypeIndex
+// // // TypeIndex
 
-// export type TypeIndex = IndexStep<ArrowTypeIndex | DataTypeIndex>
-// export type ArrowTypeIndex =
-//   {
-//     case: "arrow",
-//     sub:
-//       | {case: "domain", i: number, index: TypeIndex} 
-//       | {case: "codomain", index: DataTypeIndex | HereIndex}
-//   }
-// export type DataTypeIndex =
-//   {
-//     case: "data",
-//     sub:
-//       | {case: "label"}
-//   }
+// // export type TypeIndex = IndexStep<ArrowTypeIndex | DataTypeIndex>
+// // export type ArrowTypeIndex =
+// //   {
+// //     case: "arrow",
+// //     sub:
+// //       | {case: "domain", i: number, index: TypeIndex} 
+// //       | {case: "codomain", index: DataTypeIndex | HereIndex}
+// //   }
+// // export type DataTypeIndex =
+// //   {
+// //     case: "data",
+// //     sub:
+// //       | {case: "label"}
+// //   }
 
-// // BlockIndex
+// // // BlockIndex
 
-// export type BlockIndex =
-//   IndexStep<{
-//     case: "block",
-//     sub:
-//       | {case: "binding", i: number, index: BindingIndex}
-//       | {case: "body", index: TermIndex}
-//   }>
+// // export type BlockIndex =
+// //   IndexStep<{
+// //     case: "block",
+// //     sub:
+// //       | {case: "binding", i: number, index: BindingIndex}
+// //       | {case: "body", index: TermIndex}
+// //   }>
 
-// export type BindingIndex =
-//   IndexStep<{
-//     case: "binding",
-//     sub:
-//       | {case: "label"}
-//       | {case: "type", index: TypeIndex}
-//       | {case: "term", index: TermIndex}
-//   }>
+// // export type BindingIndex =
+// //   IndexStep<{
+// //     case: "binding",
+// //     sub:
+// //       | {case: "label"}
+// //       | {case: "type", index: TypeIndex}
+// //       | {case: "term", index: TermIndex}
+// //   }>
 
-// // TermIndex
+// // // TermIndex
 
-// export type TermIndex = IndexStep<LambdaTermTermIndex | NeutralTermTermIndex>
-// export type LambdaTermTermIndex =
-//   {
-//   case: "lambda",
-//   sub:
-//     | {case: "parameter", i: number, index: ParameterIndex}
-//     | {case: "body", index: TermIndex}
-//   }
-// export type NeutralTermTermIndex =
-//   {
-//     case: "neutral",
-//     sub:
-//       | {case: "applicant"}
-//       | {case: "argument", i: number, index: TermIndex}
-//   }
+// // export type TermIndex = IndexStep<LambdaTermTermIndex | NeutralTermTermIndex>
+// // export type LambdaTermTermIndex =
+// //   {
+// //   case: "lambda",
+// //   sub:
+// //     | {case: "parameter", i: number, index: ParameterIndex}
+// //     | {case: "body", index: TermIndex}
+// //   }
+// // export type NeutralTermTermIndex =
+// //   {
+// //     case: "neutral",
+// //     sub:
+// //       | {case: "applicant"}
+// //       | {case: "argument", i: number, index: TermIndex}
+// //   }
 
-// export type ParameterIndex =
-//   IndexStep<{
-//     case: "parameter", 
-//     sub:
-//       | {case: "label"}
-//       | {case: "domain", index: TypeIndex}
-//   }>
+// // export type ParameterIndex =
+// //   IndexStep<{
+// //     case: "parameter", 
+// //     sub:
+// //       | {case: "label"}
+// //       | {case: "domain", index: TypeIndex}
+// //   }>
 
-// export function pushIndex(moduleIndex: ModuleIndex, index: Index): ModuleIndex {throw new Error()}
+// // export function pushIndex(moduleIndex: ModuleIndex, index: Index): ModuleIndex {throw new Error()}
   
--- src/lang/model.ts
+++ src/lang/model.ts
@@ -1,95 +1,96 @@
-import { Map } from "immutable";
-import { Index, ModuleIndex, TypeIndex } from ".";
-import { Module, Term, Type, DataType, HoleType, Label, NeutralTerm, Block } from "./syntax";
+export type Model = {}
 
-type Change = {
-  case : "input",
-  change : Change,
-  index : number
+// import { Map } from "immutable";
+// import { Term, Type, DataType, HoleType, NeutralTerm, Block } from "./syntax";
+
+// type Change = {
+//   case : "input",
+//   change : Change,
+//   index : number
+// // } | {
+//   // case : "output"
 // } | {
-  // case : "output"
-} | {
-  case : "deleteInput",
-  index : number
-} | {
-  case : "insertInput",
-  index : number
-} | {
-  case : "changeInput",
-  index : number,
-  newType : Type
-} | {
-  case : "changeOutput",
-  newType : DataType | HoleType
-}
+//   case : "deleteInput",
+//   index : number
+// } | {
+//   case : "insertInput",
+//   index : number
+// } | {
+//   case : "changeInput",
+//   index : number,
+//   newType : Type
+// } | {
+//   case : "changeOutput",
+//   newType : DataType | HoleType
+// }
 
-type Either<a,b> = {
-  case : "left",
-  val : a
-} | {
-  case : "right",
-  val : b
-}
-type Delete = {}
-type ChangeCtx = Map<Label, Either<Change, Delete>>
+// type Either<a,b> = {
+//   case : "left",
+//   val : a
+// } | {
+//   case : "right",
+//   val : b
+// }
+// type Delete = {}
+// type ChangeCtx = Map<Label, Either<Change, Delete>>
 
-function changeTerm(term : Term, changes : ChangeCtx, change : Change) : Term {
-  if(term.case === "lambda"){
-  } else if(term.case === "neutral"){
-  } else if(term.case === "hole"){
-  }
-  throw new Error("shouldn't get here");
-}
-function changeNeutral(neu : NeutralTerm, changes : ChangeCtx, change : Change) : NeutralTerm {
-  throw new Error();
-}
-function changeBlock(neu : Block, changes : ChangeCtx, change : Change) : Block {
-  throw new Error();
-}
-function searchBlock(neu : Block, changes : ChangeCtx) : Block {
-  throw new Error();
-}
+// function changeTerm(term : Term, changes : ChangeCtx, change : Change) : Term {
+//   if(term.case === "lambda"){
+//   } else if(term.case === "neutral"){
+//   } else if(term.case === "hole"){
+//   }
+//   throw new Error("shouldn't get here");
+// }
+// function changeNeutral(neu : NeutralTerm, changes : ChangeCtx, change : Change) : NeutralTerm {
+//   throw new Error();
+// }
+// function changeBlock(neu : Block, changes : ChangeCtx, change : Change) : Block {
+//   throw new Error();
+// }
+// function searchBlock(neu : Block, changes : ChangeCtx) : Block {
+//   throw new Error();
+// }
 
-// Remove the statement at `index`.
-export function removeStatement(module: Module, index: ModuleIndex): Module {throw new Error()}
+// // Remove the statement at `index`.
+// export function removeStatement(module: Module, index: ModuleIndex): Module {throw new Error()}
 
-// Remove the constructor at `index`.
-export function removeConstructor(module: Module, index: ModuleIndex): Module {throw new Error()}
+// // Remove the constructor at `index`.
+// export function removeConstructor(module: Module, index: ModuleIndex): Module {throw new Error()}
 
-// Remove the binding at `index`.
-export function removeBinding(module: Module, index: ModuleIndex): Module {throw new Error()}
+// // Remove the binding at `index`.
+// export function removeBinding(module: Module, index: ModuleIndex): Module {throw new Error()}
 
-// Insert `domain` at `domainIndex` relative to the top of the type of the
-// binding at `parentIndex`.
-export function insertDomain(
-  module: Module,
-  parentIndex: ModuleIndex,
-  domainIndex: TypeIndex,
-  domain: Type
-): Module {throw new Error()}
+// // Insert `domain` at `domainIndex` relative to the top of the type of the
+// // binding at `parentIndex`.
+// export function insertDomain(
+//   module: Module,
+//   parentIndex: ModuleIndex,
+//   domainIndex: TypeIndex,
+//   domain: Type
+// ): Module {throw new Error()}
 
-// Remove the domain at `domainIndex` relative to the top of the type of the
-// binding at `parentIndex`.
-export function removeDomain(
-  module: Module,
-  parentIndex: ModuleIndex,
-  domainIndex: TypeIndex
-): Module {throw new Error()}
+// // Remove the domain at `domainIndex` relative to the top of the type of the
+// // binding at `parentIndex`.
+// export function removeDomain(
+//   module: Module,
+//   parentIndex: ModuleIndex,
+//   domainIndex: TypeIndex
+// ): Module {throw new Error()}
 
-// Move the domain at `sourceDomainIndex` to `targetDomainIndex`, which are
-// domains of the arrow type at `arrowIndex`, relative to the top of the type of
-// the binding at `parentIndex`.
-export function moveDomain(
-  module: Module,
-  parentIndex: ModuleIndex,
-  arrowIndex: TypeIndex,
-  sourceDomainIndex: number,
-  targetDomainIndex: number
-): Module {throw new Error()}
+// // Move the domain at `sourceDomainIndex` to `targetDomainIndex`, which are
+// // domains of the arrow type at `arrowIndex`, relative to the top of the type of
+// // the binding at `parentIndex`.
+// export function moveDomain(
+//   module: Module,
+//   parentIndex: ModuleIndex,
+//   arrowIndex: TypeIndex,
+//   sourceDomainIndex: number,
+//   targetDomainIndex: number
+// ): Module {throw new Error()}
 
-// Fill the hole at `index` with `term`
-export function fillHoleTerm(module: Module, index: Index, term: Term): Module {throw new Error()}
+// // // Fill the hole at `index` with `term`
+// // export function fillHoleTerm(module: Module, index: Index, term: Term): Module {throw new Error()}
 
-// Dig a hole at `index`.
-export function digHoleTerm(module: Module, index: Index): Module {throw new Error()}
+// // // Dig a hole at `index`.
+// // export function digHoleTerm(module: Module, index: Index): Module {throw new Error()}
 
