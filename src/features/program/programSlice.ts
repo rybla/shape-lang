af7993c807836bffb98e2f70ccf753630463a2e4
Henry Blanchette <henry@monkeyface.com>
2022-02-20 21:53:01 -0500

    turns out that the index wasn't good enough before

--- src/features/program/programSlice.ts
+++ src/features/program/programSlice.ts
@@ -1,32 +1,27 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { Map } from "immutable";
-import { lookupAt, ModuleIndex, replaceAt } from "../../lang";
-import { Format } from "../../lang/format";
-import { insertDomain, moveDomain, removeBinding, removeConstructor, removeDomain, removeStatement } from "../../lang/model";
-import { ArrowType, Binding, Block, Constructor, Context, DataDefinition,  FormatField, freshBlock, freshHoleTerm, freshHoleType, freshLabel, Index, Syntax, Label, LambdaTerm, Parameter, Statement, Term, TermDefinition, Type } from "../../lang/syntax";
-
+import { here, Index } from "../../lang";
+import { defaultFormat, Format, FormatField } from "../../lang/format";
+import { Block, freshBlock, Id, Name, Syntax } from "../../lang/syntax";
 export default programSlice
 
 // ProgramState
 
 export type ProgramState = {
-  module: Format<Module>,
-  focus: ModuleIndex,
+  module: Format<Block>,
+  focus: Index<Block>,
   mode: Mode
 }
 
 export type Mode = 
   | {case: "edit"}
-  | {case: "label", label: Label}
+  | {case: "label"}
 
 // initialState
 
 const initialState: ProgramState = {
-  module: {
-    case: "module",
-    statements: []
-  },
-  focus: "top",
+  module: defaultFormat(freshBlock()),
+  focus: here,
   mode: {case: "edit"}
 }
 
@@ -37,160 +32,29 @@ export const programSlice = createSlice({
   initialState,
   reducers: {
     // module
-    manipulateStatements: (state, action: PayloadAction<{manipulation: ArrayManipulation, case: "data definition" | "term definition"}>) => {
-      applyArrayManipulation(
-        state,
-        state.module.statements,
-        action.payload.manipulation,
-        {
-          make: (m) => {
-            switch (action.payload.case) {
-              case "data definition": return {case: "data definition", label: freshLabel(), constructors: [] } as DataDefinition
-              case "term definition": return { case: "term definition", label: freshLabel(), type: freshHoleType(), block: freshBlock()} as TermDefinition
-            }
-          },
-          remove: (m) => {
-            state.module = removeStatement(state.module, state.focus as ModuleIndex)
-            state.focus = moveIndex(state.module, state.focus, "up")
-          },
-          move: (m) => {throw new Error()}
-        }
-      )
-    },
+    manipulateStatements: (state, action: PayloadAction<{manipulation: ArrayManipulation, case: "data definition" | "term definition"}>) => {},
     // data definition
-    manipulateConstructors: (state, action: PayloadAction<{manipulation: ArrayManipulation}>) => {
-      applyArrayManipulation(
-        state,
-        lookupAt<Module, DataDefinition>(state.module, state.focus, Map()).target.constructors,
-        action.payload.manipulation,
-        {
-          make: (m) => ({case: "constructor", label: freshLabel(), domains: []} as Constructor),
-          remove: (m) => {
-            state.module = removeConstructor(state.module, state.focus as ModuleIndex)
-            state.focus = moveIndex(state.module, state.focus, "up")
-          },
-          move: (m) => {throw new Error()}
-        },
-      )
-    },
+    manipulateConstructors: (state, action: PayloadAction<{manipulation: ArrayManipulation}>) => {},
     // constructor
-    manipulateDomains: (state, action: PayloadAction<{manipulation: ArrayManipulation}>) => {
-      function splitAtTypeIndex(index: Index): {parentIndex: ModuleIndex, typeIndex: TypeIndex} {throw new Error()}
-      function lastIndex<I extends Index>(index: Index): I {throw new Error()}
-      switch (action.payload.manipulation.case) {
-        case "insert": {
-          let {parentIndex, typeIndex} = splitAtTypeIndex(state.focus)
-          state.module = insertDomain(state.module, parentIndex, typeIndex, freshHoleType())
-          break
-        }
-        case "remove": {
-          let {parentIndex, typeIndex} = splitAtTypeIndex(state.focus)
-          state.module = removeDomain(state.module, parentIndex, typeIndex)
-          break
-        }
-        case "move": {
-          let {parentIndex, typeIndex} = splitAtTypeIndex(state.focus)
-          state.module = moveDomain(state.module, parentIndex, typeIndex, action.payload.manipulation.i, action.payload.manipulation.j)
-          break
-        }
-      }
-    },
+    manipulateDomains: (state, action: PayloadAction<{manipulation: ArrayManipulation}>) => {},
     // block
-    manipulateBindings: (state, action: PayloadAction<{manipulation: ArrayManipulation}>) => {
-      applyArrayManipulation(
-        state,
-        lookupAt<Module, Block>(state.module, state.focus, Map()).target.bindings,
-        action.payload.manipulation,
-        {
-          make: (m) => ({case: "binding", label: freshLabel(), type: freshHoleType(), term: freshHoleTerm()} as Binding),
-          remove: (m) => {
-            state.module = removeBinding(state.module, state.focus as ModuleIndex)
-            state.focus = moveIndex(state.module, state.focus, "up")
-          },
-          move: (m) => {throw new Error()}
-        }
-      )
-    },
+    manipulateBindings: (state, action: PayloadAction<{manipulation: ArrayManipulation}>) => {},
     // term
-    fillLambdaTerm: (state) => {
-      replaceAt<Module, Term>(
-        state.module,
-        state.focus,
-        Map(),
-        (target, gamma) => ({
-            case: "lambda",
-            parameters: [],
-            body: freshBlock()
-        })
-      )
-    },
-    fillNeutralTerm: (state, action: PayloadAction<{label: Label, argCount: number}>) => {
-      let args: Term[] = [];
-      for (let i = 0; i < action.payload.argCount; i++) args.push(freshHoleTerm())
-      replaceAt<Module, Term>(
-        state.module,
-        state.focus,
-        Map(),
-        (target, gamma) => ({
-          case: "neutral",
-          applicant: action.payload.label,
-          args
-        })
-      )
-    },
-    dig: (state) => {
-      replaceAt<Module, Term>(
-        state.module,
-        state.focus,
-        Map(),
-        (target, gamma) => freshHoleTerm()
-      )
-    },
+    fillLambdaTerm: (state) => {},
+    fillNeutralTerm: (state, action: PayloadAction<{id: Id, argCount: number}>) => {},
+    dig: (state) => {},
     // label
-    appendLabel: (state, action: PayloadAction<string>) => {
-      switch (state.mode.case) {
-        case "label": {
-          state.mode.label.value = state.mode.label.value.concat(action.payload)
-          break
-        }
-        default: break;
-      }
-    },
-    backspaceLabel: (state) => {
-      switch (state.mode.case) {
-        case "label": {
-          state.mode.label.value = state.mode.label.value.slice(0, state.mode.label.value.length - 2)
-          break
-        }
-        default: break;
-      }
-    },
+    appendLabel: (state, action: PayloadAction<string>) => {},
+    backspaceLabel: (state) => {},
     // format
-    toggleFormatAttribute: (state, action: PayloadAction<FormatField>) => {
-      let node = lookupAt<Module, Syntax>(state.module, state.focus, Map()).target
-      if (!node.format.has(action.payload)) node.format.set(action.payload, false)
-      node.format.set(action.payload, !node.format.get(action.payload))
-    },
-    moveFocus: (state, action: PayloadAction<NavigationDirection>) => {
-      state.focus = moveIndex(state.module, state.focus, action.payload)
-    },
+    toggleFormatAttribute: (state, action: PayloadAction<FormatField>) => {},
+    moveFocus: (state, action: PayloadAction<NavigationDirection>) => {},
     // mode
     editMode: (state) => {
-      state.mode = {
-        case: "edit"
-      }
+      state.mode = {case: "edit"}
     },
     labelMode: (state) => {
-      let label =
-        lookupAt<Module, Label>(
-          state.module,
-          state.focus,
-          Map()
-        ).target
-      state.mode = {
-        case: "label",
-        label
-      }
+      state.mode = {case: "label"}
     },
   }
 })
@@ -202,38 +66,36 @@ export type InsertArrayManipulation = {case: "insert", i: number}
 export type RemoveArrayManipulation = {case: "remove", i: number}
 export type MoveArrayManipulation = {case: "move", i: number, j: number}
 
-function applyArrayManipulation<A>(
-  state: ProgramState,
-  array: A[],
-  manipulation: ArrayManipulation,
-  handlers: {
-    make: (m: InsertArrayManipulation) => A,
-    remove: (m: RemoveArrayManipulation) => void,
-    move: (m: MoveArrayManipulation) => void
-  }
-): void {
-  switch (manipulation.case) {
-    case "insert": {
-      let a: A = handlers.make(manipulation)
-      array.splice(manipulation.i, 0, a)
-      break
-    }
-    case "remove": {
-      handlers.remove(manipulation)
-      break
-    }
-    case "move": {
-      handlers.move(manipulation)
-      break
-    }
-  }
-}
+// function applyArrayManipulation<A>(
+//   state: ProgramState,
+//   array: A[],
+//   manipulation: ArrayManipulation,
+//   handlers: {
+//     make: (m: InsertArrayManipulation) => A,
+//     remove: (m: RemoveArrayManipulation) => void,
+//     move: (m: MoveArrayManipulation) => void
+//   }
+// ): void {
+//   switch (manipulation.case) {
+//     case "insert": {
+//       let a: A = handlers.make(manipulation)
+//       array.splice(manipulation.i, 0, a)
+//       break
+//     }
+//     case "remove": {
+//       handlers.remove(manipulation)
+//       break
+//     }
+//     case "move": {
+//       handlers.move(manipulation)
+//       break
+//     }
+//   }
+// }
 
 // Navigation
 
 export type NavigationDirection = "up" | "down" | "left" | "right" | "next" | "previous" | "top"
 
-export function moveIndex(module: Module, index: Index, direction: NavigationDirection): Index {
-  throw new Error("unimplemented")
-}
+export function moveIndex<S1 extends Syntax, S2 extends Syntax>(s: S1, index: Index<S1>, direction: NavigationDirection): S2 {throw new Error()}
 
--- src/lang/index.ts
+++ src/lang/index.ts
@@ -15,6 +15,7 @@ type IndexTerminal =
   | Reference
 
 export type IndexHere = {case: "here"}
+export const here: IndexHere = {case: "here"}
 
 export type IndexStep<S extends Syntax, Key extends keyof S> =
   S extends IndexTerminal ? IndexHere :
@@ -26,50 +27,46 @@ export type IndexStep<S extends Syntax, Key extends keyof S> =
 
 // If only I could write generative sum types...
 export type Index<S extends Syntax> = 
-  | {case: "here"}
+  | IndexHere
   | // Block
-    S extends Block ? 
-      ( IndexStep<Block, "definitions">
-      | IndexStep<Block, "body"> ) :
-    // Definition
-    S extends TermDefinition ?
-      ( IndexStep<TermDefinition, "uniqueBinding"> 
-      | IndexStep<TermDefinition, "type"> 
-      | IndexStep<TermDefinition, "term"> ) :
-    S extends DataDefinition ?
-      ( IndexStep<DataDefinition, "id">
-      | IndexStep<DataDefinition, "constructors"> ) :
-    S extends Constructor ?
-      ( IndexStep<Constructor, "uniqueBinding"> 
-      | IndexStep<Constructor, "parameters"> ) :
-    // Type
-    S extends ArrowType ?
-      ( IndexStep<ArrowType, "parameters">
-      | IndexStep<ArrowType, "output"> ) :
-    S extends DataType ? IndexHere :
-    S extends HoleType ? IndexHere :
-    // Term
-    S extends LambdaTerm ?
-      ( IndexStep<LambdaTerm, "ids">
-      | IndexStep<LambdaTerm, "block"> ) :
-    S extends NeutralTerm ?
-      ( IndexStep<NeutralTerm, "reference">
-      | IndexStep<NeutralTerm, "args"> ) :
-    S extends MatchTerm ?
-      ( IndexStep<MatchTerm, "reference">
-      | IndexStep<MatchTerm, "term">
-      | IndexStep<MatchTerm, "cases"> ) :
-    S extends HoleTerm ? IndexHere :
-    S extends Case ?
-      ( IndexStep<Case, "bindings">
-      | IndexStep<Case, "block"> ) :
-    S extends Parameter ?
-      ( IndexStep<Parameter, "name">
-      | IndexStep<Parameter, "type"> ) :
-    S extends UniqueBinding ? IndexHere :
-    S extends Binding ? IndexHere :
-    S extends Reference ? IndexHere :
-    S extends Name ? IndexHere :
-    never
+    (
+      S extends Block ? 
+        ( IndexStep<Block, "definitions">
+        | IndexStep<Block, "body"> ) :
+      S extends Definition ?
+        ( IndexStep<TermDefinition, "uniqueBinding"> 
+        | IndexStep<TermDefinition, "type"> 
+        | IndexStep<TermDefinition, "term">
+        | IndexStep<DataDefinition, "id">
+        | IndexStep<DataDefinition, "constructors"> ) :
+      S extends Constructor ?
+        ( IndexStep<Constructor, "uniqueBinding"> 
+        | IndexStep<Constructor, "parameters"> ) :
+      S extends Type ?
+        ( IndexStep<ArrowType, "parameters">
+        | IndexStep<ArrowType, "output"> ) :
+      S extends Term ?
+        // LambdaTerm
+        ( IndexStep<LambdaTerm, "ids">
+        | IndexStep<LambdaTerm, "block">
+        // NeutralTerm
+        | IndexStep<NeutralTerm, "reference">
+        | IndexStep<NeutralTerm, "args"> 
+        // MatchTerm
+        | IndexStep<MatchTerm, "reference">
+        | IndexStep<MatchTerm, "term">
+        | IndexStep<MatchTerm, "cases"> ) :
+      S extends Case ?
+        ( IndexStep<Case, "bindings">
+        | IndexStep<Case, "block"> ) :
+      S extends Parameter ?
+        ( IndexStep<Parameter, "name">
+        | IndexStep<Parameter, "type"> ) :
+      S extends UniqueBinding ? IndexHere :
+      S extends Binding ? IndexHere :
+      S extends Reference ? IndexHere :
+      S extends Name ? IndexHere :
+      never
+    )
     
 export function concatIndex<S1 extends Syntax, S2 extends Syntax>(i1: Index<S1>, i2: Index<S2>): Index<S1> {throw new Error()}
